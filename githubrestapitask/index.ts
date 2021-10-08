71a847c327c588b41e8d39c365b85a94e7abb53d
Seva Alekseyev <sevaa@nih.gov>
2021-10-08 14:35:21 -0400

    Scalar value extraction from the result JSON

--- githubrestapitask/index.ts
+++ githubrestapitask/index.ts
@@ -24,6 +24,76 @@ if(ignoreSslErrors != null)
 
 var httpCallbackClient = new httpClient.HttpClient(tl.getVariable("AZURE_HTTP_USER_AGENT"), undefined, requestOptions);
 
+/*
+Path goes:
+$.member.arrayMember.0.property
+Or, if the root JSON value is expected to be an array:
+$.0.member
+
+Negative array indices are supported, Python style. Array length will be added to those.
+
+The created job var name becomes whatever is after the initial "$.",
+so for path "$.foo.bar" the var name will be "foo.bar".
+
+If the variable value is an array or an object, we don't spell those out. Oh well.
+*/
+function extractValue(path : string, prefix : string, result : any) : void
+{
+    try
+    {
+        const pathParts = path.split(".");
+        let context:any = null, contextName = "";
+        for(const pathPart of pathParts)
+        { //pathPart may be an member name or an array index
+            let element:string|number = pathPart; //Member name or array index
+            if(element == "$")
+            {
+                context = result;
+                contextName = "";
+            }
+            else
+            {
+                if(!context)
+                {
+                    tl.warning("While evaluating " + path + ", the path " + contextName + " leads nowhere, can not resolve any further");
+                    return;                    
+                }
+
+                if(Array.isArray(context))
+                {
+                    element = parseInt(element); //Member is an index!
+                    if(isNaN(element))
+                    {
+                        tl.warning("While evaluating " + path + ", the member " + pathPart + " is not a valid array index");
+                        return;                    
+                    }
+                    if(element < 0) //Allow for negative indices to extract the last element
+                        element += context.length;                    
+                }
+                else if(!(element in context))
+                {
+                    tl.warning("While evaluating " + path + ", unable to resolve " + element + " under context " + contextName);
+                    return;
+                }
+
+                context = context[element];
+                if(contextName != "")
+                    contextName += ".";
+                contextName += pathPart;
+            }
+        }
+
+        //Finally! Context is the path target now.
+        const varName = prefix + contextName, value = context.toString();
+        console.log("Value extracted: " + varName + "=" + value);
+        tl.setVariable(varName, value);
+    }
+    catch(err: any)
+    {
+        tl.warning("While evaluating " + path + ", got at error: " + err.message);
+    }
+}
+
 async function run() {
     try {        
         // Get basic task inputs
@@ -47,6 +117,16 @@ async function run() {
              var result = JSON.stringify(resbody);
              console.log("Response: " + result);
              tl.setVariable('Output', result);
+
+             const valuesToExtract = tl.getInput("valuesToExtract");
+             if(valuesToExtract)
+             {
+                const prefix = tl.getInput("valuePrefix"),
+                    resObj = JSON.parse(resbody);
+                for(const path of valuesToExtract.split("\n"))
+                    if(path) //Allow for blank strings in the middle
+                        extractValue(path.trim(), prefix ? prefix : "", resObj);
+             }
          }
           
          let ignoreFailure: boolean = tl.getBoolInput("ignoreFailure");
@@ -55,7 +135,7 @@ async function run() {
             tl.setResult(tl.TaskResult.Failed, "Github api call was unsuccessfull");
          }
     }
-    catch (err) {
+    catch (err:any) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
 }
